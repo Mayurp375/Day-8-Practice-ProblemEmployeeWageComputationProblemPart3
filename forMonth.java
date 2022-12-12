364f47fba19e334d8cda4f21e67e9bf9dd3db6ef
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-12 14:48:36 +0530

    monthly wage

---
+++ forMonth.java
@@ -0,0 +1,35 @@
+package classProblemByAmolMateSir.Day8PracticeProblem.EmployeeWage;
+
+import java.util.Random;
+
+public class forMonth {
+    static void check(){
+        System.out.println("Welcome to Employee Wage");
+
+        int perHr = 20;
+        int dayHr = 8;
+        int Month = 20;
+        Random random = new Random();
+        int present = random.nextInt(3);
+        switch (present) {
+            case 1:
+                perHr = 20;
+                dayHr = 8;
+                System.out.println("present");
+                break;
+            case 2:
+                perHr = 0;
+                dayHr = 0;
+                System.out.println("absent");
+                break;
+            default:
+                perHr = 20;
+                dayHr = 4;
+                System.out.println("part time");
+        }
+        int dailyWage = perHr * dayHr;
+        Month = dailyWage*Month;
+        System.out.println("daily wage is : " + dailyWage);
+        System.out.println("Monthly wage is : " + Month);
+    }
+}
